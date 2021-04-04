2d5066ea87f45b5f734370aa510ddff938b4d9f3
IanJTyson <ian@morphosis.org>
2021-04-04 11:18:05 -0700

    2.2 updated

--- sketch.js
+++ sketch.js
@@ -1,36 +1,65 @@
 
 let planets = [{
   name: "Suul",
-  color: "Red"
+  char: "Hot... very very hot."
 }, {
   name: "Mevy",
-  color: "Brown"
+  char: "Basically a big ol' rock."
 }, {
   name: "Veis",
-  color: "Neon Green"
+  char: "Don't you know that it's toxic?"
 }, {
-  name: "X-02",
-  color: "Silver"
+  name: "XAErf-02",
+  char: "Founded by Elon's child."
 }, {
   name: "Jurt",
-  color: "Blue"
+  char: "Stormy AF, don't bring a ship here lad!"
 }];
 
 let randomIndex;
+let animating = false;
 
 function setup() {
   createCanvas(600, 600);
   background(220);
+textSize(32);
 
+text("Click to begin your journey!", 50, 50);
+
+//setTimeout(changeBackground, 1000);
+//setInterval(changeBackground, 1000);
 }
 
 function draw() {
 
+  if(animating == true){
+    ellipse(width *.5, height *.5, random (20, 300))
+  }
+
+}
+
+//function changeBackground(){
+//  background(random(255), (random(255), (random(255));
+//}
+
+function randomizer(){
+  animating = false;
+  if (planets[0]){
+
+    background(220);
+    randomIndex = int(random(planets.length));
+    text(`${planets[randomIndex].name} - characteristic:
+${planets[randomIndex].char}`, 50, 50);
+    //text(planets[randomIndex].name + " - characteristic: " + planets[randomIndex].char, 50, 50);
+    planets.splice(randomIndex, 1);
+  } else {
+    background(220);
+    text("No more planets to visit!", 50, 50);
+  }
 }
 
 function mousePressed() {
-  background(220);
-  randomIndex = int(random(planets.length));
-  text(planets[randomIndex].name, 50, 50);
-  planets.splice(randomIndex, 1);
+  animating = true;
+setTimeout(randomizer, 2000);
+
 }
