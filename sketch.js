a9b14085574f15b6b17f4863ab05517e290dac69
IanJTyson <ian@morphosis.org>
2021-04-04 17:11:25 -0700

    2.3 updated

--- sketch.js
+++ sketch.js
@@ -19,30 +19,49 @@ let planets = [{
 let randomIndex;
 let animating = false;
 let planet = [];
+let imageCounter = 0;
+let button;
+// let img0;
+// let img1;
+// let img2;
+// let img3;
+// let img4;
 
 function preload(){
 
 for (let i = 0; i <= 4; i++){
-  planet[i] = loadImage("Images/planet_" + i + ".jpg")
+  planet[i] = loadImage("Images/planet_" + i + ".jpg");
+  //img1 = loadImage('Images/planet_0.jpg');
+
+
+
+
 }
 
 }
 
 function setup() {
-  createCanvas(600, 600);
-  background(220);
-textSize(32);
-
-text("Click to begin your journey!", 50, 50);
-
-//setTimeout(changeBackground, 1000);
-//setInterval(changeBackground, 1000);
+  createCanvas(800, 800);
+  background(0);
+  fill(255);
+textSize(24);
+imageMode(CENTER);
+//text("Click to begin your journey!", 50, 50);
+frameRate(8);
+button = createButton("Click to begin your journey!");
+button.mousePressed(buttonPressed);
 }
 
 function draw() {
 
   if(animating == true){
-    image(planet[0], width/2, height/2);
+    image(planet[imageCounter], width/2, height/2);
+
+    if(imageCounter < planet.length - 1){
+    imageCounter++;
+  } else {
+    imageCounter = 0;
+  }
   }
 
 }
@@ -55,20 +74,24 @@ function randomizer(){
   animating = false;
   if (planets[0]){
 
-    background(220);
+    background(0);
     randomIndex = int(random(planets.length));
+    //random number tied to a planet
     text(`${planets[randomIndex].name} - characteristic:
+      
 ${planets[randomIndex].char}`, 50, 50);
+image(random(planet), width/2, height/2);
     //text(planets[randomIndex].name + " - characteristic: " + planets[randomIndex].char, 50, 50);
     planets.splice(randomIndex, 1);
   } else {
-    background(220);
+    background(0);
     text("No more planets to visit!", 50, 50);
   }
 }
 
-function mousePressed() {
+function buttonPressed() {
   animating = true;
-setTimeout(randomizer, 2000);
+setTimeout(randomizer, 1000);
+background (0);
 
 }
